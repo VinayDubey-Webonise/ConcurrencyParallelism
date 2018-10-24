0ae4c924ca5b4fba278352b91002185a184cff1b
VinayDubeyV-Webonise <43874875+VinayDubeyV-Webonise@users.noreply.github.com>
2018-10-24 12:02:44 +0530

    Delete Fork.java

--- Fork.java
+++
@@ -1,9 +0,0 @@
-public class Fork {
-    int forkId;
-    boolean forkStatus;
-
-    public Fork(int forkId, boolean forkStatus) {
-        this.forkId = forkId;
-        this.forkStatus = forkStatus;
-    }
-}
